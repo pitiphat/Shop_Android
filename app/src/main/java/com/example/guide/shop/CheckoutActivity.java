00be4cb9e8ffbf9f21bff8c88bce9c640d9c147e
pitiphat <pitiphatmaprajak@gmail.com>
2017-06-07 00:09:14 +0700

    add checkout order

---
+++ app/src/main/java/com/example/guide/shop/CheckoutActivity.java
@@ -0,0 +1,37 @@
+package com.example.guide.shop;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class CheckoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Intent _inboundindex = getIntent();
+        String _tableID = _inboundindex.getStringExtra("table_id");
+
+        Toast.makeText(getApplicationContext(), _tableID, Toast.LENGTH_SHORT).show();
+
+    }
+
+}
